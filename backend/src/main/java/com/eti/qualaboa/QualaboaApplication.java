a091cbd8a8a2debcc89a680afaef1ec4927a0a82
dcarm3 <dcardosom3@gmail.com>
2025-09-02 19:33:33 -0300

    feat | 0000 | inicializando projeto backend

---
+++ backend/src/main/java/com/eti/qualaboa/QualaboaApplication.java
@@ -0,0 +1,13 @@
+package com.eti.qualaboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualaboaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualaboaApplication.class, args);
+	}
+
+}
